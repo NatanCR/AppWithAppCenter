983ecbd439c6d507b86fd82e94f3a30701139942
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-05-16 14:47:26 -0300

    initial commit

---
+++ MacroChallenge/MacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeApp.swift
+//  MacroChallenge
+//
+//  Created by Natan de Camargo Rodrigues on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
