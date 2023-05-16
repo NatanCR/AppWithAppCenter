099ad87c560cd1f575b89d215877f6da58afb67b
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-05-16 15:38:54 -0300

    app import for app center analytics

--- MacroChallenge/MacroChallengeApp.swift
+++ MacroChallenge/MacroChallengeApp.swift
@@ -5,6 +5,9 @@
 //  Created by Natan de Camargo Rodrigues on 16/05/23.
 //
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 import SwiftUI
 
 @main
@@ -13,5 +16,12 @@ struct MacroChallengeApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    init(){
+        AppCenter.start(withAppSecret: "4f3b4894-823d-4288-9930-0aef316b737e", services:[
+          Analytics.self,
+          Crashes.self
+        ])
     }
 }
